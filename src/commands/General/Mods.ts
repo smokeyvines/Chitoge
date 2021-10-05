af2c3d52f56ee578de9b930a07a1da604ac2ec74
smokeyvines <88516210+smokeyvines@users.noreply.github.com>
2021-10-05 14:25:51 +0200

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -17,12 +17,12 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '🌼 *Moderators* 💮\n\n'
+        let text = '🌺 *Moderators* 🌺\n\n'
         filteredMap.forEach(
             (user, index) =>
                 (text += `#${index + 1}\n🌺 *Username: ${
                     user.notify || user.vname || user.name || 'null'
-                }*\n🌼 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
+                }*\n🌺 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
         text += `\n🌺 Offset hacker 🌺 `
         return void M.reply(text)
